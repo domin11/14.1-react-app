51f59861f729a2d6548dd751b9d7e726b32646b8
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-04 12:33:23 +0200

    Add images

--- script.js
+++ script.js
@@ -2,34 +2,40 @@ var movies = [
   {
 	id: 1,  
     title: 'Harry Potter',
-    desc: 'film o czarodzieju'
+    desc: 'film o czarodzieju',
+	img: './images/harry.jpg'
   },
   {
 	id: 2,  
     title: 'Król Lew',
-    desc: 'Film o królu sawanny'
+    desc: 'Film o królu sawanny',
+	img: './images/lion.jpg'
   },
   {
 	id: 3,  
-    title: 'Harry Potter',
-    desc: 'film o czarodzieju'
+    title: 'Toy Story',
+    desc: 'film o żywych zabawkach',
+	img: './images/toy.jpg'
   },
   {
 	id: 4,  
-    title: 'Harry Potter',
-    desc: 'film o czarodzieju'
+    title: 'Piraci z Karaibów',
+    desc: 'film o piratach',
+	img: './images/pirates.jpg'
   },
   {
 	id: 5,  
-    title: 'Harry Potter',
-    desc: 'film o czarodzieju'
+    title: 'Shrek',
+    desc: 'film o zielonym potworze',
+	img: './images/shrek.jpg'
   }
 ];
 
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc)
+      React.createElement('p', {}, movie.desc),
+	  React.createElement('img', {src: movie.img})
     );
 });
 
