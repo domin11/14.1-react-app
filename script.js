041cf16c0877046ed02803b1a187d589beb00552
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-04 12:02:37 +0200

    Add index.html & script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+var movies = [
+  {
+	id: 1,  
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju'
+  },
+  {
+	id: 2,  
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny'
+  },
+  {
+	id: 3,  
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju'
+  },
+  {
+	id: 4,  
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju'
+  },
+  {
+	id: 5,  
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju'
+  }
+];
+
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc)
+    );
+});
+
+var element =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements)
+  );
+
+ReactDOM.render(element, document.getElementById('app'));
